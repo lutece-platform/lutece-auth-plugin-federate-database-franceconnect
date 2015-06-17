ec0b0fc1487f6bf7d5f7ca1ef58c371bd72ff2bd
Merlin <francoiseric.merlin@paris.fr>
2015-06-17 18:10:14 +0200

    FEDCONNECT-1 - Init Project

--- src/sql/plugins/federatedatabasefranceconnect/plugin/create_db_federate_fc.sql
+++ src/sql/plugins/federatedatabasefranceconnect/plugin/create_db_federate_fc.sql
@@ -5,5 +5,5 @@ DROP TABLE IF EXISTS federate_fc_link;
 CREATE TABLE federate_fc_link (
     lutece_user_name varchar(255)DEFAULT '' NOT NULL,
   	federate_key varchar(255)DEFAULT '' NOT NULL,
-  	PRIMARY KEY (lutece_user_name,federate_key)
+  	PRIMARY KEY (federate_key)
   );
